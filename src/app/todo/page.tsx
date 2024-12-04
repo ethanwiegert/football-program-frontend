604528bbbb9a3031975c4fee147ff1c583118ab8
Ethan Wiegert <ewiegert99@gmail.com>
2024-12-04 18:41:22 -0500

    Initial ToDo page draft

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,5 +1,22 @@
 export default function todolist() {
     return(
+        <>
         <h3 className="">To Do Page</h3>
+        <ol>
+            <li>
+                <input type="checkbox"/>
+                Initial Item
+            </li>
+            <li>
+                <input type="checkbox"/>
+                Second Item
+            </li>
+            <li>
+                <input type="checkbox"/>
+                Third Item Item
+            </li>
+        </ol>
+        </>
+        
     )
 }
